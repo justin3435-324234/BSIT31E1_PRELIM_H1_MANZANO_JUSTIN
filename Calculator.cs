bc2ec1d9d0af5e142e1e779170916367f01b8121
Cholo230125 <miguel07.ramos85@gmail.com>
2026-06-21 22:04:25 +0800

    Add files via upload
    Signed-off-by: Cholo230125 <miguel07.ramos85@gmail.com>

---
+++ Calculator.cs
@@ -0,0 +1,12 @@
+﻿class Calculator
+{
+    public double Calculate(double num1, double num2, string op)
+    {
+        if (op == "+") return num1 + num2;
+        if (op == "-") return num1 - num2;
+        if (op == "*") return num1 * num2;
+        if (op == "/") return num1 / num2;
+
+        return double.NaN;
+    }
+}
